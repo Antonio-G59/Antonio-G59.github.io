56bfcaa25fabf614878898b3af17e4ed84b45426
Antonio-G59 <167109356+Antonio-G59@users.noreply.github.com>
2025-04-27 21:07:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ async function runExample() {
      x[23] = document.getElementById('sexual_content').value;
      x[24] = document.getElementById('sexual_themes').value;
      x[25] = document.getElementById('simulated_gambling').value;
-     x[26] = document.getElementById('strong_janguage').value;
+     x[26] = document.getElementById('strong_language').value;
      x[27] = document.getElementById('strong_sexual_content').value;
      x[28] = document.getElementById('suggestive_themes').value;
      x[29] = document.getElementById('use_of_alcohol').value;
