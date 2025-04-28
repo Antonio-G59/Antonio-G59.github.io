b7ca473eb75c15d65a4f260d30ea99a92a3e5b47
Antonio-G59 <167109356+Antonio-G59@users.noreply.github.com>
2025-04-27 21:15:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,64 +1,48 @@
-
 async function runExample() {
+    let inputIds = [
+        'console', 'alcohol_reference', 'animated_blood', 'blood', 'blood_and_gore',
+        'cartoon_violence', 'crude_humor', 'drug_reference', 'fantasy_violence', 'intense_violence',
+        'language', 'lyrics', 'mature_humor', 'mild_blood', 'mild_cartoon_violence',
+        'mild_fantasy_violence', 'mild_language', 'mild_lyrics', 'mild_suggestive_themes',
+        'mild_violence', 'no_descriptors', 'nudity', 'partial_nudity', 'sexual_content',
+        'sexual_themes', 'simulated_gambling', 'strong_language', 'strong_sexual_content',
+        'suggestive_themes', 'use_of_alcohol', 'use_of_drugs_and_alcohol', 'violence'
+    ];
 
-    var x = new Float32Array( 1, 32 )
+    let x = new Float32Array(32);
 
-    var x = [];
-
-     x[0] = document.getElementById('console').value;
-     x[1] = document.getElementById('alcohol_reference').value;
-     x[2] = document.getElementById('animated_blood').value;
-     x[3] = document.getElementById('blood').value;
-     x[4] = document.getElementById('blood_and_gore').value;
-     x[5] = document.getElementById('cartoon_violence').value;
-     x[6] = document.getElementById('crude_humor').value;
-     x[7] = document.getElementById('drug_reference').value;
-     x[8] = document.getElementById('fantasy_violence').value;
-     x[9] = document.getElementById('intense_violence').value;
-     x[10] = document.getElementById('language').value;
-     x[11] = document.getElementById('lyrics').value;
-     x[12] = document.getElementById('mature_humor').value;
-     x[13] = document.getElementById('mild_blood').value;
-     x[14] = document.getElementById('mild_cartoon_violence').value;
-     x[15] = document.getElementById('mild_fantasy_violence').value;
-     x[16] = document.getElementById('mild_language').value;
-     x[17] = document.getElementById('mild_lyrics').value;
-     x[18] = document.getElementById('mild_suggestive_themes').value;
-     x[19] = document.getElementById('mild_violence').value;
-     x[20] = document.getElementById('no_descriptors').value;
-     x[21] = document.getElementById('nudity').value;
-     x[22] = document.getElementById('partial_nudity').value;
-     x[23] = document.getElementById('sexual_content').value;
-     x[24] = document.getElementById('sexual_themes').value;
-     x[25] = document.getElementById('simulated_gambling').value;
-     x[26] = document.getElementById('strong_language').value;
-     x[27] = document.getElementById('strong_sexual_content').value;
-     x[28] = document.getElementById('suggestive_themes').value;
-     x[29] = document.getElementById('use_of_alcohol').value;
-     x[30] = document.getElementById('use_of_drugs_and_alcohol').value;
-     x[31] = document.getElementById('violence').value;
+    for (let i = 0; i < inputIds.length; i++) {
+        let element = document.getElementById(inputIds[i]);
+        if (element) {
+            x[i] = parseFloat(element.value) || 0;
+        } else {
+            console.error(`Missing element with id '${inputIds[i]}'`);
+            x[i] = 0;
+        }
+    }
 
     let tensorX = new onnx.Tensor(x, 'float32', [1, 32]);
 
     let session = new onnx.InferenceSession();
-
     await session.loadModel("./DLnet_gamerating.onnx");
-    let outputMap = await session.run([tensorX]);
+
+    let outputMap = await session.run({ input: tensorX });
     let outputData = outputMap.get('output1');
 
-   let predictions = document.getElementById('predictions');
+    let predictions = document.getElementById('predictions');
 
-   let ratings = ["E", "ET", "T", "M"];
-   let ratingIndex = Math.round(outputData.data[0]);
+    let ratings = ["E", "ET", "T", "M"];
+    let ratingIndex = Math.round(outputData.data[0]);
+    if (ratingIndex < 0 || ratingIndex >= ratings.length) {
+        ratingIndex = 0;
+    }
 
-predictions.innerHTML = ` <hr> Game Rating Prediction: <br/>
-   <table>
-     <tr>
-       <td>  Predicted Rating  </td>
-       <td id="td0">  ${ratings[ratingIndex]}  </td>
-     </tr>
-  </table>`;
-    
-
-
+    predictions.innerHTML = `
+        <hr> Game Rating Prediction: <br/>
+        <table>
+            <tr>
+                <td>Predicted Rating</td>
+                <td id="td0">${ratings[ratingIndex]}</td>
+            </tr>
+        </table>`;
 }
